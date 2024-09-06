0df75f5857c2be7564404bfc5324cf4739a5770c
nickel <nicolas.wenzel@gmx.de>
2024-09-06 22:28:40 +0200

    build: add requirements for imgui and lager and cmake infrastructure

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,7 @@
+import os
+
 from conan import ConanFile
+from conan.tools.files import copy
 
 
 class Recipe(ConanFile):
@@ -9,9 +12,20 @@ class Recipe(ConanFile):
         self.folders.generators = "build/conan"
 
     def requirements(self):
-        self.requires("fmt/11.0.2")
+        self.requires("cereal/1.3.2")
+        self.requires("sdl/2.30.7")
+        self.requires("immer/0.8.1")
         self.requires("lager/0.1.1")
         self.requires("imgui/1.91.0-docking")
 
     def build_requirements(self):
         self.test_requires("catch2/3.7.0")
+
+    def configure(self):
+        self.options["sdl"].pulse = False # Avoid problems with clang-18
+
+    def generate(self):
+        copy(self, "*sdl*", os.path.join(self.dependencies["imgui"].package_folder,
+            "res", "bindings"), os.path.join(self.source_folder, "bindings"))
+        copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder,
+            "res", "bindings"), os.path.join(self.source_folder, "bindings"))
