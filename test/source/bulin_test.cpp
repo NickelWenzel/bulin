30c446e35452e0f8a94277fa5fd63bd83f428876
nickel <nicolas.wenzel@gmx.de>
2024-09-06 22:27:58 +0200

    chore:nothing to test so far

--- test/source/bulin_test.cpp
+++ test/source/bulin_test.cpp
@@ -1,9 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include "lib.hpp"
-
 TEST_CASE("Name is bulin", "[library]")
 {
-  auto const lib = library {};
-  REQUIRE(lib.name == "bulin");
+  REQUIRE(true);
 }
