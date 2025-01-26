c5856f1d0464fa5ac61f9799ea66dcec12420517
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-01-26 22:23:52 +0100

    chore: try to merge iced examples editor and custom_shader in one application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> iced::Result {
+    iced::application(
+        "Custom Shader - Iced",
+        bulin_rust::Application::update,
+        bulin_rust::Application::view,
+    )
+    .theme(Editor::theme)
+    .font(include_bytes!("../fonts/icons.ttf").as_slice())
+    .default_font(Font::MONOSPACE)
+    .subscription(bulin_rust::Application::subscription)
+    .run()
 }
