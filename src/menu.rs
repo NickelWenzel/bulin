c46c9497e98a493318bda49352ad7923a3f50382
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-02-15 18:20:55 +0100

    fix: styling, okay-ish now

--- src/menu.rs
+++ src/menu.rs
@@ -1,8 +1,8 @@
-use iced::border::Radius;
-use iced::widget::{button, text};
+use iced::widget::{button, text, Button};
 use iced::{Border, Color, Element, Length};
 
 use iced_aw::menu::{self, Item};
+use iced_aw::style::menu_bar::primary;
 use iced_aw::{menu_bar, menu_items};
 use iced_aw::{quad, widgets::InnerBounds};
 
@@ -18,35 +18,46 @@ pub enum Message {
 
 #[rustfmt::skip]
 pub fn view() -> Element<'static, Message> {
-    let menu_template = |items| menu::Menu::new(items).max_width(180.0).offset(6.0);
+    let menu_template = |items| menu::Menu::new(items).max_width(180.0);
     menu_bar!(
-        (button(text("File")), menu_template(
+        (main_menu_item("File"), menu_template(
             menu_items!(
-                (button(text("Open File")).on_press(Message::OpenFile))
-                (button(text("Save File")).on_press(Message::SaveFile))
+                (menu_item("Open File", Message::OpenFile))
+                (menu_item("Save File", Message::SaveFile))
                 (separator())
-                (button(text("Open Project")).on_press(Message::OpenProject))
-                (button(text("Save Project")).on_press(Message::SaveProject))
+                (menu_item("Open Project", Message::OpenProject))
+                (menu_item("Save Project", Message::SaveProject))
             )
         ))
-        (button(text("Edit")), menu_template(
+        (main_menu_item("Edit"), menu_template(
             menu_items!(
-                (button(text("Undo")).on_press(Message::Undo))
-                (button(text("Redo")).on_press(Message::Redo))
+                (menu_item("Undo", Message::Undo))
+                (menu_item("Redo", Message::Redo))
             )
         ))
-    ).into()
+    )
+    .draw_path(menu::DrawPath::Backdrop)
+    .style(primary).into()
+}
+
+fn main_menu_item(item_text: &str) -> Button<'_, Message> {
+    button(text(item_text))
+        .style(button::text)
+}
+
+fn menu_item(item_text: &str, message: Message) -> Button<'_, Message> {
+    button(text(item_text))
+        .style(button::text)
+        .on_press(message)
+        .width(Length::Fill)
 }
 
 fn separator() -> quad::Quad {
     quad::Quad {
         quad_color: Color::from([0.5; 3]).into(),
-        quad_border: Border {
-            radius: Radius::new(4.0),
-            ..Default::default()
-        },
-        inner_bounds: InnerBounds::Ratio(0.98, 0.2),
-        height: Length::Fixed(20.0),
+        quad_border: Border::default(),
+        inner_bounds: InnerBounds::Ratio(0.995, 0.05),
+        height: Length::Fixed(1.),
         ..Default::default()
     }
 }
