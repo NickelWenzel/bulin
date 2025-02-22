d1c1e361edf429f6702c16d9badfa6caed2da709
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-02-22 02:13:00 +0100

    feat: started working on uniform editor widgets

--- src/uniforms_editor.rs
+++ src/uniforms_editor.rs
@@ -2,21 +2,33 @@ mod time;
 pub mod uniform;
 
 use crate::pipeline_update::*;
-use uniform::Uniform;
+use std::time::Instant;
+use uniform::*;
 
-use iced::{widget::container, Element, Task};
+use iced::{
+    widget::{button, container, horizontal_space, row},
+    Element, Task,
+};
 use serde::{Deserialize, Serialize};
 use std::vec::Vec;
 
 #[derive(Debug, Clone)]
 pub enum Message {
+    Time(time::Message),
+    Uniforms(u32, uniform::Message),
+    Candidate(uniform::CandidateMessage),
     Update(PipelineUpdate),
+    AddTime,
+    RemoveTime,
+    AddUniform(Uniform),
+    RemoveUniform(u32),
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct UniformsEditor {
     uniforms: Vec<Uniform>,
-    current_add_candidate: Uniform,
+    #[serde(skip, default = "Candidate::default")]
+    candidate: Candidate,
     time: Option<time::Time>,
 }
 
@@ -24,19 +36,92 @@ impl UniformsEditor {
     pub fn new() -> Self {
         Self {
             uniforms: Vec::new(),
-            current_add_candidate: Uniform {
-                value: uniform::Type::Int,
-                name: String::new(),
-            },
+            candidate: Candidate::new(),
             time: Option::None,
         }
     }
 
     pub fn update(&mut self, message: Message) -> Task<Message> {
-        return Task::none();
+        match message {
+            Message::AddTime => {
+                let now = Instant::now();
+                self.time = Some(time::Time::new(now));
+                Task::done(Message::Update(PipelineUpdate::Time(TimeUpdate::Add(now))))
+            }
+            Message::RemoveTime => {
+                self.time = Option::None;
+                Task::done(Message::Update(PipelineUpdate::Time(TimeUpdate::Remove)))
+            }
+            Message::Time(message) => {
+                if let Some(time) = &mut self.time {
+                    time.update(message).map(Message::Time)
+                } else {
+                    Task::none()
+                }
+            }
+            Message::AddUniform(uniform) => {
+                self.uniforms.push(uniform.clone());
+                Task::done(Message::Update(PipelineUpdate::Uniforms(
+                    UniformsUpdate::Add(uniform),
+                )))
+            }
+            Message::RemoveUniform(idx) => {
+                self.uniforms.swap_remove(idx as usize);
+                Task::done(Message::Update(PipelineUpdate::Uniforms(
+                    UniformsUpdate::Remove(idx),
+                )))
+            }
+            Message::Uniforms(idx, message) => {
+                if let Some(uniform) = self.uniforms.get_mut(idx as usize) {
+                    uniform
+                        .update(message)
+                        .map(move |m| Message::Uniforms(idx, m))
+                        .chain(Task::done(Message::Update(PipelineUpdate::Uniforms(
+                            UniformsUpdate::Update(idx, uniform.clone()),
+                        ))))
+                } else {
+                    Task::none()
+                }
+            }
+            Message::Candidate(message) => self.candidate.update(message).map(Message::Candidate),
+            Message::Update(_) => Task::none(),
+        }
     }
 
     pub fn view(&self) -> Element<Message> {
-        container(iced::widget::column![]).into()
+        let time = if let Some(time) = &self.time {
+            row![
+                time.view().map(Message::Time),
+                button("X").on_press(Message::RemoveTime)
+            ]
+        } else {
+            row![horizontal_space(), button("+").on_press(Message::AddTime)]
+        };
+
+        let add_candidate_button = if let Ok(uniform) = self.candidate.clone().try_into() {
+            button("+").on_press(Message::AddUniform(uniform))
+        } else {
+            button("+")
+        };
+
+        let candidate = row![
+            self.candidate.view().map(Message::Candidate),
+            add_candidate_button,
+        ];
+
+        let uniforms = iced::widget::Column::from_vec(
+            self.uniforms
+                .iter()
+                .enumerate()
+                .map(|(idx, u)| {
+                    row![
+                        u.view().map(move |m| Message::Uniforms(idx as u32, m)),
+                        button("X").on_press(Message::RemoveUniform(idx as u32)),
+                    ]
+                    .into()
+                })
+                .collect(),
+        );
+        iced::widget::column![time, candidate, uniforms].into()
     }
 }
--- src/uniforms_editor/uniform.rs
+++ src/uniforms_editor/uniform.rs
@@ -1,4 +1,11 @@
-use iced::{Element, Task};
+use std::ops::RangeBounds;
+
+use iced::{
+    widget::{column, combo_box, horizontal_space, row, text, text_input},
+    Element, Task, Length,
+};
+use iced_aw::number_input;
+use num_traits::bounds::Bounded;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone)]
@@ -7,23 +14,53 @@ pub enum Message {
     ChangeName(String),
 }
 
-#[derive(Serialize, Deserialize)]
-#[derive(Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Type {
-    Int,
-    Float,
-    VecFloat2,
-    VecFloat3,
-    Col3,
-    VecFloat4,
-    Col4,
-    VecInt2,
-    VecInt3,
-    VecInt4,
+    Int(i32),
+    Float(f32),
+    VecFloat2((f32, f32)),
+    VecFloat3((f32, f32, f32)),
+    Col3((f32, f32, f32)),
+    VecFloat4((f32, f32, f32, f32)),
+    Col4((f32, f32, f32, f32)),
+    VecInt2((i32, i32)),
+    VecInt3((i32, i32, i32)),
+    VecInt4((i32, i32, i32, i32)),
 }
 
-#[derive(Debug, Clone)]
-#[derive(Serialize, Deserialize)]
+impl Type {
+    pub const ALL: [Type; 10] = [
+        Type::Int(0),
+        Type::Float(0.0),
+        Type::VecFloat2((0.0, 0.0)),
+        Type::VecFloat3((0.0, 0.0, 0.0)),
+        Type::Col3((0.5, 0.5, 0.5)),
+        Type::VecFloat4((0.0, 0.0, 0.0, 0.0)),
+        Type::Col4((0.5, 0.5, 0.5, 1.0)),
+        Type::VecInt2((0, 0)),
+        Type::VecInt3((0, 0, 0)),
+        Type::VecInt4((0, 0, 0, 0)),
+    ];
+}
+
+impl std::fmt::Display for Type {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Self::Int(_) => "Int",
+            Self::Float(_) => "Float",
+            Self::VecFloat2(_) => "VecFloat2",
+            Self::VecFloat3(_) => "VecFloat3",
+            Self::Col3(_) => "Col3",
+            Self::VecFloat4(_) => "VecFloat4",
+            Self::Col4(_) => "Col4",
+            Self::VecInt2(_) => "VecInt2",
+            Self::VecInt3(_) => "VecInt3",
+            Self::VecInt4(_) => "VecInt4",
+        })
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Uniform {
     pub value: Type,
     pub name: String,
@@ -31,10 +68,218 @@ pub struct Uniform {
 
 impl Uniform {
     pub fn update(&mut self, message: Message) -> Task<Message> {
-        return Task::none();
+        match message {
+            Message::ChangeValue(value) => {
+                self.value = value;
+                Task::none()
+            }
+            Message::ChangeName(name) => {
+                self.name = name;
+                Task::none()
+            }
+        }
     }
 
     pub fn view(&self) -> Element<Message> {
-        Element::new(iced::widget::column![])
+        let name = text(&self.name);
+        let value_view = match &self.value {
+            Type::Int(value) => {
+                number_input(value, -100..100, |v| Message::ChangeValue(Type::Int(v))).into()
+            }
+            Type::Float(value) => number_input(value, -100.0..100.0, |v| {
+                Message::ChangeValue(Type::Float(v))
+            })
+            .into(),
+            Type::VecFloat2(value) => Self::number_input2(value, -100.0..100.0, |v| {
+                Message::ChangeValue(Type::VecFloat2(v))
+            }),
+            Type::VecFloat3(value) => Self::number_input3(value, -100.0..100.0, |v| {
+                Message::ChangeValue(Type::VecFloat3(v))
+            }),
+            Type::Col3(value) => {
+                Self::number_input3(value, 0.0..1.0, |v| Message::ChangeValue(Type::Col3(v)))
+            }
+            Type::VecFloat4(value) => Self::number_input4(value, -100.0..100.0, |v| {
+                Message::ChangeValue(Type::VecFloat4(v))
+            }),
+            Type::Col4(value) => {
+                Self::number_input4(value, 0.0..1.0, |v| Message::ChangeValue(Type::Col4(v)))
+            }
+            Type::VecInt2(value) => {
+                Self::number_input2(value, -100..100, |v| Message::ChangeValue(Type::VecInt2(v)))
+            }
+            Type::VecInt3(value) => {
+                Self::number_input3(value, -100..100, |v| Message::ChangeValue(Type::VecInt3(v)))
+            }
+            Type::VecInt4(value) => {
+                Self::number_input4(value, -100..100, |v| Message::ChangeValue(Type::VecInt4(v)))
+            }
+        };
+        row![name, horizontal_space(), value_view].into()
+    }
+
+    fn number_input2<'a, T, F>(
+        (v0, v1): &'a (T, T),
+        bounds: impl RangeBounds<T> + Clone,
+        on_change: F,
+    ) -> Element<'a, Message>
+    where
+        F: 'a + Fn((T, T)) -> Message + Copy,
+        T: 'a
+            + num_traits::Num
+            + num_traits::NumAssignOps
+            + PartialOrd
+            + std::fmt::Display
+            + std::str::FromStr
+            + Copy
+            + Bounded,
+    {
+        row![
+            number_input(v0, bounds.clone(), move |v| on_change((v, *v1))),
+            number_input(v1, bounds, move |v| on_change((*v0, v))),
+        ]
+        .into()
+    }
+
+    fn number_input3<'a, T, F>(
+        (v0, v1, v2): &'a (T, T, T),
+        bounds: impl RangeBounds<T> + Clone,
+        on_change: F,
+    ) -> Element<'a, Message>
+    where
+        F: 'a + Fn((T, T, T)) -> Message + Copy,
+        T: 'a
+            + num_traits::Num
+            + num_traits::NumAssignOps
+            + PartialOrd
+            + std::fmt::Display
+            + std::str::FromStr
+            + Copy
+            + Bounded,
+    {
+        row![
+            number_input(v0, bounds.clone(), move |v| on_change((
+                v,
+                v1.clone(),
+                v2.clone()
+            ))),
+            number_input(v1, bounds.clone(), move |v| on_change((
+                v0.clone(),
+                v,
+                v2.clone()
+            ))),
+            number_input(v2, bounds, move |v| on_change((v0.clone(), v1.clone(), v))),
+        ]
+        .into()
+    }
+
+    fn number_input4<'a, T, F>(
+        (v0, v1, v2, v3): &'a (T, T, T, T),
+        bounds: impl RangeBounds<T> + Clone,
+        on_change: F,
+    ) -> Element<'a, Message>
+    where
+        F: 'a + Fn((T, T, T, T)) -> Message + Copy,
+        T: 'a
+            + num_traits::Num
+            + num_traits::NumAssignOps
+            + PartialOrd
+            + std::fmt::Display
+            + std::str::FromStr
+            + Copy
+            + Bounded,
+    {
+        row![
+            number_input(v0, bounds.clone(), move |v| on_change((
+                v,
+                v1.clone(),
+                v2.clone(),
+                v3.clone()
+            ))),
+            number_input(v1, bounds.clone(), move |v| on_change((
+                v0.clone(),
+                v,
+                v2.clone(),
+                v3.clone()
+            ))),
+            number_input(v1, bounds.clone(), move |v| on_change((
+                v0.clone(),
+                v1.clone(),
+                v,
+                v3.clone()
+            ))),
+            number_input(v2, bounds, move |v| on_change((
+                v0.clone(),
+                v1.clone(),
+                v2.clone(),
+                v
+            ))),
+        ]
+        .into()
+    }
+}
+
+impl TryFrom<Candidate> for Uniform {
+    type Error = &'static str;
+
+    fn try_from(candidate: Candidate) -> Result<Self, Self::Error> {
+        if let Some(value) = candidate.current_type {
+            Ok(Self {
+                value,
+                name: candidate.name,
+            })
+        } else {
+            Err("Value not present.")
+        }
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct Candidate {
+    types: combo_box::State<Type>,
+    pub current_type: Option<Type>,
+    pub name: String,
+}
+
+#[derive(Debug, Clone)]
+pub enum CandidateMessage {
+    Selected(Type),
+    NameChanged(String),
+}
+
+impl Candidate {
+    pub fn new() -> Self {
+        Self {
+            types: combo_box::State::new(Type::ALL.to_vec()),
+            current_type: None,
+            name: String::from("Name"),
+        }
+    }
+
+    pub fn view(&self) -> Element<CandidateMessage> {
+        let upper_left = row![
+            combo_box(
+                &self.types,
+                "Select type",
+                self.current_type.as_ref(),
+                CandidateMessage::Selected,
+            ),
+            text_input("Name...", &self.name).on_input(CandidateMessage::NameChanged)
+        ];
+        let lower_left = row![];
+        column![upper_left, lower_left].height(Length::Shrink).into()
+    }
+
+    pub fn update(&mut self, message: CandidateMessage) -> Task<CandidateMessage> {
+        match message {
+            CandidateMessage::Selected(value) => {
+                self.current_type = Some(value);
+                Task::none()
+            }
+            CandidateMessage::NameChanged(name) => {
+                self.name = name;
+                Task::none()
+            }
+        }
     }
 }
